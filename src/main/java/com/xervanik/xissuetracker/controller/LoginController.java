15ce30dc0e05b3bb7a6d11734af40d30d537e805
John Pettit <john.pettit@tridonic.com>
2018-11-15 08:11:28 -0800

    Security

--- src/main/java/com/xervanik/xissuetracker/controller/LoginController.java
+++ src/main/java/com/xervanik/xissuetracker/controller/LoginController.java
@@ -5,9 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-    @RequestMapping({"/", "", "/index"})
+public class LoginController {
+    @RequestMapping({"/login"})
     public String getIndex(Model model) {
-        return "index";
+        return "login";
     }
 }
