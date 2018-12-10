bb2d8759fe899c3fa49c877010cc8d798fd30906
John Pettit <john.pettit@tridonic.com>
2018-12-10 09:55:28 -0800

    Logging added

--- src/main/java/com/xervanik/xissuetracker/service/IssueServiceImpl.java
+++ src/main/java/com/xervanik/xissuetracker/service/IssueServiceImpl.java
@@ -2,6 +2,8 @@ package com.xervanik.xissuetracker.service;
 
 import com.xervanik.xissuetracker.dao.Issue;
 import com.xervanik.xissuetracker.repositories.IssueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -9,6 +11,9 @@ import java.util.List;
 
 @Service
 public class IssueServiceImpl implements IssueService {
+
+    Logger logger = LoggerFactory.getLogger(IssueServiceImpl.class);
+
     private IssueRepository issueRepo;
 
     @Autowired
@@ -18,6 +23,7 @@ public class IssueServiceImpl implements IssueService {
 
     @Override
     public List<Issue> getAll() {
+        logger.info("Getting all Issues");
         List<Issue> issues = new ArrayList<>();
         issueRepo.findAll().forEach(issues::add);
         return issues;
@@ -25,21 +31,25 @@ public class IssueServiceImpl implements IssueService {
 
     @Override
     public Issue getById(Integer id) {
+        logger.info("Getting 1 Issues   id : "  + id.toString());
         return issueRepo.queryById(id);
     }
 
     @Override
     public Issue addNew(Issue issue) {
+        logger.info("Creating Issue");
         return issueRepo.save(issue);
     }
 
     @Override
     public Issue edit(Issue issue) {
+        logger.info("Editing Issue");
         return issueRepo.save(issue);
     }
 
     @Override
     public void delete(Issue issue) {
+        logger.info("Deleting Issue");
         issueRepo.delete(issue);
     }
 }
